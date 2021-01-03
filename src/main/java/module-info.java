033ea47c13fb147f888451bbb7bd819244804009
ilciro <danielecinus10@gmail.com>
2021-01-03 11:14:36 -0300

    ins

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,17 @@ module com.example.test {
 	requires itextpdf;
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
+	exports application;
+	opens application to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
+	exports factoryBook;
+	opens factoryBook to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
+	exports pagamento;
+	opens pagamento to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
+	exports database;
+	opens database to javafx.graphics,javafx.base,javafx.controls,javafx.fxml,java.sql;
+	
+	//exports reports;
+	
 }
