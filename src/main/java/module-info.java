12b39d4d858869acc9467328fe455286bf6a7674
ginoz <ginoz@Lenovo-14-Gino.station>
2021-01-03 12:52:30 +0100

    fxml sistemato

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,18 @@ module com.example.test {
 	requires itextpdf;
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.base;
+	exports application;
+	opens application to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
+	exports factoryBook;
+	opens factoryBook to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
+	exports database;
+	opens database to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
+	exports loginSingleton to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
+	exports pagamento to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
+	exports factoryUser to javafx.graphics,javafx.base,javafx.controls,javafx.fxml;
+
+
 }
+
